9731211b0d6f787b3ec02ac291af4204a1382e8e
Mohamed Elawadi <mohamed@elawadi.net>
2016-04-01 18:30:19 +0200

    accept git repos as an argument

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require('path')
 var colors = require('colors')
 var replHistory = require('repl.history')
 var exec = require('child_process').exec
+var fs = require('fs')
 
 const TRYMODULE_PATH = process.env.TRYMODULE_PATH || path.resolve((process.env.HOME || process.env.USERPROFILE), '.trymodule')
 const TRYMODULE_HISTORY_PATH = process.env.TRYMODULE_HISTORY_PATH || path.resolve(TRYMODULE_PATH, 'repl_history')
@@ -19,19 +20,31 @@ const takePackageArguments = (args) => args.splice(2)
 
 const packageLocation = (pkg) => path.resolve(TRYMODULE_PATH, 'node_modules', pkg)
 
+const packageName = (pkg) => {
+  if (~pkg.indexOf('/')) {
+    name = pkg.split('/').filter(x => x).pop()
+    name = name.endsWith('.git')?name.slice(0,-4):name
+  }
+  else {
+    name = pkg
+  }
+  return name
+}
+
 const loadPackage = (pkg) => {
   return new Promise((resolve, reject) => {
+    const name = packageName(pkg)
     try {
-      const loadedPackage = require(packageLocation(pkg))
-      console.log(colors.blue(`'${pkg}' was already installed since before!`))
-      resolve({name: pkg, package: loadedPackage})
+      const loadedPackage = require(packageLocation(name))
+      console.log(colors.blue(`'${name}' was already installed since before!`))
+      resolve({name: name, package: loadedPackage})
     } catch (err) {
-      console.log(colors.yellow(`Couldn't find '${pkg}' locally, gonna download it now`))
+      console.log(colors.yellow(`Couldn't find '${name}' locally, gonna download it now`))
       npmi({name: pkg, path: TRYMODULE_PATH}, (err, result) => {
         if (err) {
           console.log(colors.red(err.message))
           if (err.statusCode === 404) {
-            throw new Error(`Could not find package ${pkg}`)
+            throw new Error(`Could not find package ${name}`)
           }
           if (err.code === npmi.LOAD_ERR) {
             throw new Error('npm load error')
@@ -40,8 +53,10 @@ const loadPackage = (pkg) => {
             throw new Error('npm install error')
           }
         }
-        const loadedPackage = require(packageLocation(pkg))
-        resolve({name: pkg, package: loadedPackage})
+        fs.symlink(result.pop()[0].split('@')[0], packageLocation(name), (err) => {
+          const loadedPackage = require(packageLocation(name))
+          resolve({name: name, package: loadedPackage})
+        })
       })
     }
   })
